97ec623cb2dd0ce19e32dcb3c0ae205481501fdc
Anej Svete <anej.svete@protonmail.com>
2025-11-23 16:28:02 +0100

    udpate

--- scripts/launch_smart.sh
+++ scripts/launch_smart.sh
@@ -81,13 +81,13 @@ for lang in "${LANGUAGES[@]}"; do
     if [ "$lang" = "arithmetic" ] || [ "$lang" = "bfvp" ]; then
         # SHORT_LENGTHS[$lang]=150
         # LONG_LENGTHS[$lang]=300
-        SHORT_LENGTHS[$lang]=300
-        LONG_LENGTHS[$lang]=600
+        SHORT_LENGTHS[$lang]=80
+        LONG_LENGTHS[$lang]=160
     else
         # SHORT_LENGTHS[$lang]=35
         # LONG_LENGTHS[$lang]=70
-        SHORT_LENGTHS[$lang]=70
-        LONG_LENGTHS[$lang]=140
+        SHORT_LENGTHS[$lang]=35
+        LONG_LENGTHS[$lang]=110
     fi
 done
 
