576d8ba6ffecad4ad4f44283e591982b752797d3
Anej Svete <anej.svete@protonmail.com>
2025-11-27 17:06:55 +0100

    Update

--- scripts/launch_smart.sh
+++ scripts/launch_smart.sh
@@ -79,11 +79,11 @@ declare -A LONG_LENGTHS
 
 for lang in "${LANGUAGES[@]}"; do
     if [ "$lang" = "arithmetic" ] || [ "$lang" = "bfvp" ]; then
-        SHORT_LENGTHS[$lang]=256
-        LONG_LENGTHS[$lang]=512
+        SHORT_LENGTHS[$lang]=128
+        LONG_LENGTHS[$lang]=256
     else
-        SHORT_LENGTHS[$lang]=256
-        LONG_LENGTHS[$lang]=512
+        SHORT_LENGTHS[$lang]=128
+        LONG_LENGTHS[$lang]=256
     fi
 done
 
